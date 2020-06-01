7fb5bc8e765f43f0905669711148256f69574b03
ProjectManEdge <laf16003@byui.edu>
2020-06-01 10:10:52 -0600

    Added a comment

--- app/src/main/java/ion/hyperon/groupproject/MainActivity.java
+++ app/src/main/java/ion/hyperon/groupproject/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Little Bow Peep ran from the hordes.
     }
 }
