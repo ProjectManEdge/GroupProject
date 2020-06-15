86dea4fd3a84675c1ea384eb1a2c8f1f3697c653
ProjectManEdge <laf16003@byui.edu>
2020-06-15 10:13:33 -0600

    Added json saving and loading for catalog filtering

--- app/src/main/java/ion/hyperon/groupproject/MainActivity.java
+++ app/src/main/java/ion/hyperon/groupproject/MainActivity.java
@@ -2,7 +2,14 @@ package ion.hyperon.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
 
 // Little Bow Peep ran from the hordes.
 // Except the hordes chased her down and captured her.
@@ -15,11 +22,36 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String preferenceKey = "GraphicCardCatalog";
+    SharedPreferences preferences;
+    HashMap<String, Object> filters;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferences = getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
 
+        // load in filter information
+       loadFilter();
+
+    }
+
+    public void loadFilter() {
+
+        Gson gson = new Gson();
+        String json = preferences.getString("filter","");
+        java.lang.reflect.Type type = new TypeToken<HashMap<String, Object>>(){}.getType();
+        filters = gson.fromJson(json, type);
+    }
+
+    public void SaveFilter() {
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(filters);
+        editor.putString("filter",json);
+        editor.apply();     // This line is IMPORTANT !!!
     }
 }
