b4acf318fba6e14e7135c77543a6c8984fb95969
ProjectManEdge <laf16003@byui.edu>
2020-06-16 16:02:37 -0600

    Implemented Card Catalog saving and loading

--- app/src/main/java/ion/hyperon/groupproject/MainActivity.java
+++ app/src/main/java/ion/hyperon/groupproject/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-//import com.google.gson.Gson;
-//import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 // Little Bow Peep ran from the hordes.
 // Except the hordes chased her down and captured her.
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     static final String preferenceKey = "GraphicCardCatalog";
     SharedPreferences preferences;
+    List<GraphicCard> catalog;
     HashMap<String, Object> filters;
 
     @Override
@@ -33,25 +36,80 @@ public class MainActivity extends AppCompatActivity {
 
         preferences = getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
 
-        // load in filter information
-//       loadFilter();
+        // load in catalog, create new if it doesn't exist
+        boolean catalogExists = loadCatalog();
+        if (catalogExists == false)
+            catalog = new ArrayList<GraphicCard>();
+
+        // load in filter information, create new if it doesn't exist
+        boolean filterUsed = loadFilter();
+        if (filterUsed == false)
+            filters = new HashMap<String, Object>();
+
+
+        GraphicCard fancy = new GraphicCard();
+        fancy.name = "Elite Card";
+        fancy.price = 1000000; // ! million dollars!
+        catalog.add(fancy);
 
     }
-/*
-    public void loadFilter() {
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (!filters.isEmpty())
+            saveFilter();
+
+        if (!catalog.isEmpty())
+            saveCatalog();
+    }
+
+    public boolean loadFilter() {
 
         Gson gson = new Gson();
         String json = preferences.getString("filter","");
+
+        // No filters were used, so there is nothing to load.
+        if (json.isEmpty())
+            return false;
+
         java.lang.reflect.Type type = new TypeToken<HashMap<String, Object>>(){}.getType();
         filters = gson.fromJson(json, type);
+
+        return true;
     }
 
-    public void SaveFilter() {
+    public void saveFilter() {
 
         SharedPreferences.Editor editor = preferences.edit();
         Gson gson = new Gson();
         String json = gson.toJson(filters);
         editor.putString("filter",json);
         editor.apply();     // This line is IMPORTANT !!!
-    }*/
+    }
+
+    public void saveCatalog() {
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(catalog);
+        editor.putString("catalog",json);
+        editor.apply();     // This line is IMPORTANT !!!
+    }
+
+    public boolean loadCatalog() {
+
+        Gson gson = new Gson();
+        String json = preferences.getString("catalog","");
+
+        // No filters were used, so there is nothing to load.
+        if (json.isEmpty())
+            return false;
+
+        java.lang.reflect.Type type = new TypeToken<ArrayList<GraphicCard>>(){}.getType();
+        catalog = gson.fromJson(json, type);
+
+        return true;
+    }
 }
